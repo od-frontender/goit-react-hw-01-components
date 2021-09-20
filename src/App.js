0a39614d0c49b0226ba0549a1e81e9feca670826
od-frontender <od.frontender@gmail.com>
2021-09-20 14:17:56 +0300

    фикс

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return;
-}
+import Profile from "./components/Profile";
+import user from "./Data/user.json";
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
+}
